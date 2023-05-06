03eeb610bf62f1dd28a62161fad2e38a369257c6
fahadmustjab <beingfahad786@gmail.com>
2023-05-06 17:13:38 +0500

    second commit

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -1,5 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:jobs_app/controllers/user_controller.dart';
 import 'package:jobs_app/models/user.dart' as model;
+import 'package:uuid/uuid.dart';
 
-class FireStoreMethods {}
+class FireStoreMethods {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> postJob() async {
+    final userProvider = Get.find<UserProvider>();
+    final model.User user = userProvider.getUser;
+    String postId = const Uuid().v1();
+    try {
+      _firestore
+          .collection('jobs')
+          .doc(user.uid)
+          .collection('jobs')
+          .doc(postId)
+          .set({});
+    } catch (e) {
+      print(e);
+    }
+  }
+}
