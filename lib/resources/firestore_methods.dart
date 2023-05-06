575f5f9d5339232834c2f5a008c9cbb3bd42c446
fahadmustjab <beingfahad786@gmail.com>
2023-05-06 17:02:03 +0500

    login signup

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:jobs_app/models/user.dart' as model;
+
+class FireStoreMethods {}
